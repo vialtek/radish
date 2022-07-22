63e45d8f52fb71b0e4a1bc34b6c548586229c9e1
David Filip <davidfilip@gmail.com>
2022-07-22 15:29:33 +0200

    Model input should take just array of float

--- examples/simple_model/main.go
+++ examples/simple_model/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"gonum.org/v1/gonum/mat"
-
 	"github.com/vialtek/radish/radish"
 )
 
@@ -12,8 +10,6 @@ func main() {
 	model.AddLayer(radish.NewDenseLayer(4, 4, "relu"))
 	model.AddLayer(radish.NewDenseLayer(4, 2, "relu"))
 
-	inputData := mat.NewDense(1, 4, []float64{1, 1, 0, 0})
-	outputData := model.Evaluate(inputData)
-
-	radish.PrintMatrix(outputData, "Output")
+	modelOutput := model.Evaluate([]float64{1, 1, 0, 0})
+	radish.PrintMatrix(modelOutput, "Output")
 }
--- radish/sequential_model.go
+++ radish/sequential_model.go
@@ -20,8 +20,8 @@ func (m *SequentialModel) AddLayer(newLayer layer) {
 	m.layers = append(m.layers, newLayer)
 }
 
-func (m *SequentialModel) Evaluate(input *mat.Dense) *mat.Dense {
-	curY := input
+func (m *SequentialModel) Evaluate(input []float64) *mat.Dense {
+	curY := mat.NewDense(1, len(input), input)
 
 	for _, layer := range m.layers {
 		curY = layer.ForwardProp(curY)
