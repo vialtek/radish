d3d9ac7ae4b5d92f9ebce3873b1e030c7eeee5a7
David Filip <davidfilip@gmail.com>
2022-07-25 15:47:59 +0200

    Gradient for backward pass

--- radish/dense_layer.go
+++ radish/dense_layer.go
@@ -8,26 +8,46 @@ type DenseLayer struct {
 	activation string
 	Weights    *mat.Dense
 	Biases     *mat.Dense
+
+	forwardIn   mat.Dense
+	forwardOut  mat.Dense
+	backwardIn  mat.Dense
+	backwardOut mat.Dense
 }
 
 func NewDenseLayer(inputs, outputs int, activation string) *DenseLayer {
 	return &DenseLayer{
 		activation: activation,
-		Weights:    mat.NewDense(inputs, outputs, RandArray(inputs * outputs)),
+		Weights:    mat.NewDense(inputs, outputs, RandArray(inputs*outputs)),
 		Biases:     mat.NewDense(1, outputs, RandArray(outputs)),
 	}
 }
 
 func (l *DenseLayer) ForwardProp(input *mat.Dense) *mat.Dense {
+	l.forwardIn.Copy(input)
 	var output mat.Dense
 
 	output.Mul(input, l.Weights)
 	output.Add(&output, l.Biases)
 	output.Apply(l.activationForward, &output)
 
+	l.forwardOut.Copy(&output)
 	return &output
 }
 
+// FIXME: activation is not accounted
 func (l *DenseLayer) BackwardProp(input *mat.Dense) *mat.Dense {
-	return input
+	var dL_dy, dy_dw, dy_dx, dL_dw, dL_dx mat.Dense
+
+	l.backwardIn.Copy(input)
+	
+	dL_dy.Copy(&l.backwardIn)
+	dy_dw.Copy(&l.forwardIn)
+	dy_dx.Copy(l.Weights)
+
+	dL_dw.Mul(dy_dw.T(), &dL_dy)
+	dL_dx.Mul(&dL_dy, dy_dx.T())
+
+	l.backwardOut.Copy(&dL_dx)
+	return &dL_dx
 }
