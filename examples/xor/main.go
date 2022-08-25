6940094870cf142d19f35d0ab11fa15ad3d7529a
David Filip <davidfilip@gmail.com>
2022-08-25 11:35:03 +0200

    Example model should use ReLU for demonstration purposes.

--- examples/xor/main.go
+++ examples/xor/main.go
@@ -9,11 +9,11 @@ func main() {
 	trainLabels := [][]float64{{0}, {1}, {1}, {0}}
 
 	model := radish.NewSequentialModel()
-	model.AddLayer(2, 3, "tanh")
+	model.AddLayer(2, 3, "relu")
 	model.AddLayer(3, 1, "tanh")
 
 	model.Fit(trainExamples, trainLabels, 10000)
 
 	modelOutput := model.Evaluate([]float64{1, 0})
-	radish.PrintMatrix(modelOutput, "Output (1)")
+	radish.PrintMatrix(modelOutput, "Output (should be 1)")
 }
