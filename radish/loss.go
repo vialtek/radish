5f424993e86ffd1fbe20f1d90e4423e83a0973c7
David Filip <davidfilip@gmail.com>
2023-05-05 16:32:16 +0200

    Generalize Loss Functions (#6)
    * Implementation of Cross-entropy loss function
    
    * Generalize loss function to own object.
    
    * Update examples to specify loss function

--- radish/loss.go
+++ radish/loss.go
@@ -6,7 +6,26 @@ import (
 	"gonum.org/v1/gonum/mat"
 )
 
-func SquareLossForward(predicted, actual *mat.Dense) float64 {
+type lossFunction interface {
+	Forward(predicted, actual *mat.Dense) float64
+	Backward(predicted, actual *mat.Dense) []float64
+}
+
+func NewLossFunction(name string) lossFunction {
+	switch name {
+	case "square":
+		return &SquareLoss{}
+	case "crossentropy":
+		return &CrossEntropyLoss{}
+	default:
+		// TODO: should we panic, maybe?
+		return &UnknownLoss{}
+	}
+}
+
+type SquareLoss struct{}
+
+func (l *SquareLoss) Forward(predicted, actual *mat.Dense) float64 {
 	rows, _ := predicted.Dims()
 	error := 0.0
 	for i := 0; i < rows; i++ {
@@ -16,7 +35,7 @@ func SquareLossForward(predicted, actual *mat.Dense) float64 {
 	return (1.0 / float64(rows)) * error
 }
 
-func SquareLossBackward(predicted, actual *mat.Dense) []float64 {
+func (l *SquareLoss) Backward(predicted, actual *mat.Dense) []float64 {
 	// TODO: panic when having more columns then 1 and not in same shape
 	predictedVector := ArrayFromMatrix(predicted)
 	actualVector := ArrayFromMatrix(actual)
@@ -28,4 +47,43 @@ func SquareLossBackward(predicted, actual *mat.Dense) []float64 {
 	}
 
 	return errors
+}
+
+type CrossEntropyLoss struct{}
+
+func (l *CrossEntropyLoss) Forward(predicted, actual *mat.Dense) float64 {
+	rows, _ := predicted.Dims()
+	error := 0.0
+	for i := 0; i < rows; i++ {
+		error += actual.At(i, 0) * math.Log(predicted.At(i, 0))
+	}
+
+	return -error
+}
+
+func (l *CrossEntropyLoss) Backward(predicted, actual *mat.Dense) []float64 {
+	epsilon := 0.000001
+
+	// TODO: panic when having more columns then 1 and not in same shape
+	predictedVector := ArrayFromMatrix(predicted)
+	actualVector := ArrayFromMatrix(actual)
+	rows := len(predictedVector)
+
+	errors := make([]float64, rows)
+	for i := 0; i < rows; i++ {
+		errors[i] = -(actualVector[i] / (predictedVector[i] + epsilon))
+	}
+
+	return errors
+}
+
+type UnknownLoss struct{}
+
+func (l *UnknownLoss) Forward(predicted, actual *mat.Dense) float64 {
+	return 0.0
+}
+
+func (l *UnknownLoss) Backward(predicted, actual *mat.Dense) []float64 {
+	rows, _ := predicted.Dims()
+	return ZeroArray(rows)
 }
